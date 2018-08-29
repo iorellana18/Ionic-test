43fe59e26d6bc8a6b0a01bb57562a55d9ae20495
Ian <ian.orellana@usach.cl>
2018-08-28 21:58:55 -0300

    alert example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertController: AlertController) {
 
   }
-
+  alert(){
+	let simpleAlert = this.alertController.create({
+		title: 'Super título',
+		message: 'Acabas de presionar el botón de alerta',
+		buttons: ['Ok!'] 
+	});
+	simpleAlert.present();
+  }
+  alert2(){
+	let simpleAlert2 = this.alertController.create({
+		title: 'Super título',
+		message: 'Acabas de presionar el botón de alerta',
+		inputs: [
+			{
+				name: 'Nombre',
+				placeholder: 'Nombre'
+			}
+		],
+		buttons: [
+			{
+				text: 'Cancelar'
+			},
+			{
+				text: 'guardar'
+			}
+		] 
+	});
+	simpleAlert2.present();
+  }
 }
